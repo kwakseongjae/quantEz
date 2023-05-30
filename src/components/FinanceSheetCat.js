7d8fc29521e9f3fc79a957251e8924b33f3ea8b5
kwakseongjae <gkffhdnls13@gmail.com>
2023-05-30 19:35:19 +0900

    Hmmmmm...

--- src/components/FinanceSheetCat.js
+++ src/components/FinanceSheetCat.js
@@ -18,56 +18,56 @@ import { Link } from "react-router-dom";
 const cx = classNames.bind(style);
 export const FinanceSheetCat = () => {
   return (
-    <div className={style["popular-categories"]}>
-      <div className={style.container}>
-        <div className={style.row}>
-          <div className={style["col-lg-12"]}>
-            <div className={style["section-heading"]}>
+    <div className={cx("popular-categories")}>
+      <div className={cx("container")}>
+        <div className={cx("row")}>
+          <div className={cx("col-lg-12")}>
+            <div className={cx("section-heading")}>
               <h2>재무제표 학습</h2>
               <h6>쉬운 예시와 함께 재무제표를 학습하세요!</h6>
             </div>
           </div>
-          <div className={style["col-lg-12"]}>
-            <div className={style.naccs}>
-              <div className={style.grid}>
-                <div className={style.row}>
-                  <div className={style["col-lg-3"]}>
-                    <div className={style.menu}>
-                      <div className={`${style.firstthumb} ${style.active}`}>
-                        <div className={style.thumb}>
-                          <span className={style.icon}>
+          <div className={cx("col-lg-12")}>
+            <div className={cx("naccs")}>
+              <div className={cx("grid")}>
+                <div className={cx("row")}>
+                  <div className={cx("col-lg-3")}>
+                    <div className={cx("menu")}>
+                      <div className={cx('firstthumb', 'active')}>
+                        <div className={cx("thumb")}>
+                          <span className={cx("icon")}>
                             <img src={searchIcon1} alt="hi1" />
                           </span>
                           재무제표의 기본
                         </div>
                       </div>
                       <div>
-                        <div className={style.thumb}>
-                          <span className={style.icon}>
+                        <div className={cx("thumb")}>
+                          <span className={cx("icon")}>
                             <img src={searchIcon2} alt="hi2" />
                           </span>
                           창업 1단계
                         </div>
                       </div>
                       <div>
-                        <div className={style.thumb}>
-                          <span className={style.icon}>
+                        <div className={cx("thumb")}>
+                          <span className={cx("icon")}>
                             <img src={searchIcon3} alt="hi3" />
                           </span>
                           창업 2단계
                         </div>
                       </div>
                       <div>
-                        <div className={style.thumb}>
-                          <span className={style.icon}>
+                        <div className={cx("thumb")}>
+                          <span className={cx("icon")}>
                             <img src={searchIcon4} alt="hi4" />
                           </span>
                           창업 3단계
                         </div>
                       </div>
-                      <div className={style["last-thumb"]}>
-                        <div className={style.thumb}>
-                          <span className={style.icon}>
+                      <div className={cx("last-thumb")}>
+                        <div className={cx("thumb")}>
+                          <span className={cx("icon")}>
                             <img src={searchIcon5} alt="hi5" />
                           </span>
                           창업 4단계
@@ -75,19 +75,17 @@ export const FinanceSheetCat = () => {
                       </div>
                     </div>
                   </div>
-                  <div
-                    className={`${style["col-lg-9"]} ${style["align-self-center"]}`}
-                  >
-                    <ul className={style.nacc}>
-                      <li className={style.active}>
+                  <div className={cx("col-lg-9", "align-self-center")}>
+                    <ul className={cx("nacc")}>
+                      <li className={cx("active")}>
                         <div>
-                          <div className={style.thumb}>
-                            <div className={style.row}>
+                          <div className={cx("thumb")}>
+                            <div className={cx("row")}>
                               <div
-                                className={`${style["col-lg-5"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-5", "align-self-center")}
                               >
-                                <div className={style["left-text"]}>
-                                  <h4 className={style["category-head"]}>
+                                <div className={cx("left-text")}>
+                                  <h4 className={cx("category-head")}>
                                     반드시 알아야 할 재무제표의 기본 :<br />
                                     구조와 용어 이해하기
                                   </h4>
@@ -98,21 +96,21 @@ export const FinanceSheetCat = () => {
                                     2. 재무제표를 구성하는 3가지 큰 틀 : <br />
                                     대차대조표, 손익계산서, 현금흐름표
                                   </p>
-                                  <div className={style["main-white-button"]}>
+                                  <div className={cx("main-white-button")}>
                                     <Link
-                                      className={style.FSstart}
+                                      className={cx("FSstart")}
                                       to="/financeSheetlist/financeSheet"
                                     >
-                                      <i className={style["fa fa-eye"]}></i>학습
+                                      <i className={cx("fa", "fa-eye")}></i>학습
                                       시작하기
                                     </Link>
                                   </div>
                                 </div>
                               </div>
                               <div
-                                className={`${style["col-lg-7"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-7", "align-self-center")}
                               >
-                                <div className={style["right-image"]}>
+                                <div className={cx("right-image")}>
                                   <img src={tabImg1} alt="hi6" />
                                 </div>
                               </div>
@@ -122,12 +120,12 @@ export const FinanceSheetCat = () => {
                       </li>
                       <li>
                         <div>
-                          <div className={style.thumb}>
-                            <div className={style.row}>
+                          <div className={cx("thumb")}>
+                            <div className={cx("row")}>
                               <div
-                                className={`${style["col-lg-5"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-5", "align-self-center")}
                               >
-                                <div className={style["left-text"]}>
+                                <div className={cx("left-text")}>
                                   <h4>거래를 통한 재무제표의 응용</h4>
                                   <p>
                                     애플시드(Appleseed)주식회사 창업과 운영
@@ -135,18 +133,18 @@ export const FinanceSheetCat = () => {
                                     창업 1단계: 회사 창업자금을 투자 유치하고
                                     함께 일할 직원을 고용한다.
                                   </p>
-                                  <div className={style["main-white-button"]}>
+                                  <div className={cx("main-white-button")}>
                                     <a href="#">
-                                      <i className={style["fa fa-eye"]}></i>학습
+                                      <i className={cx("fa", "fa-eye")}></i>학습
                                       시작하기
                                     </a>
                                   </div>
                                 </div>
                               </div>
                               <div
-                                className={`${style["col-lg-7"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-7", "align-self-center")}
                               >
-                                <div className={style["right-image"]}>
+                                <div className={cx("right-image")}>
                                   <img src={tabImg2} alt="Foods on the table" />
                                 </div>
                               </div>
@@ -156,12 +154,12 @@ export const FinanceSheetCat = () => {
                       </li>
                       <li>
                         <div>
-                          <div className={style.thumb}>
-                            <div className={style.row}>
+                          <div className={cx("thumb")}>
+                            <div className={cx("row")}>
                               <div
-                                className={`${style["col-lg-5"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-5", "align-self-center")}
                               >
-                                <div className={style["left-text"]}>
+                                <div className={cx("left-text")}>
                                   <h4>Best car rentals for your trips!</h4>
                                   <p>
                                     Did you know? You can get the best free HTML
@@ -169,18 +167,18 @@ export const FinanceSheetCat = () => {
                                     pages and explore fresh and latest website
                                     templates.
                                   </p>
-                                  <div className={style["main-white-button"]}>
+                                  <div className={cx("main-white-button")}>
                                     <a href="#">
-                                      <i className={style["fa fa-eye"]}></i>{" "}
+                                      <i className={cx("fa", "fa-eye")}></i>{" "}
                                       More Listing
                                     </a>
                                   </div>
                                 </div>
                               </div>
                               <div
-                                className={`${style["col-lg-7"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-7", "align-self-center")}
                               >
-                                <div className={style["right-image"]}>
+                                <div className={cx("right-image")}>
                                   <img src={tabImg3} alt="cars in the city" />
                                 </div>
                               </div>
@@ -190,12 +188,12 @@ export const FinanceSheetCat = () => {
                       </li>
                       <li>
                         <div>
-                          <div className={style.thumb}>
-                            <div className={style.row}>
+                          <div className={cx("thumb")}>
+                            <div className={cx("row")}>
                               <div
-                                className={`${style["col-lg-5"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-5", "align-self-center")}
                               >
-                                <div className={style["left-text"]}>
+                                <div className={cx("left-text")}>
                                   <h4>Shopping List: Images from Unsplash</h4>
                                   <p>
                                     Image credits go to Unsplash website that
@@ -203,18 +201,18 @@ export const FinanceSheetCat = () => {
                                     Images used in this Plot Listing template
                                     are from Unsplash.
                                   </p>
-                                  <div className={style["main-white-button"]}>
+                                  <div className={cx("main-white-button")}>
                                     <a href="#">
-                                      <i className={style["fa fa-eye"]}></i>{" "}
+                                      <i className={cx("fa", "fa-eye")}></i>{" "}
                                       Discover More
                                     </a>
                                   </div>
                                 </div>
                               </div>
                               <div
-                                className={`${style["col-lg-7"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-7", "align-self-center")}
                               >
-                                <div className={style["right-image"]}>
+                                <div className={cx("right-image")}>
                                   <img src={tabImg4} alt="Shopping Girl" />
                                 </div>
                               </div>
@@ -224,12 +222,12 @@ export const FinanceSheetCat = () => {
                       </li>
                       <li>
                         <div>
-                          <div className={style.thumb}>
-                            <div className={style.row}>
+                          <div className={cx("thumb")}>
+                            <div className={cx("row")}>
                               <div
-                                className={`${style["col-lg-5"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-5", "align-self-center")}
                               >
-                                <div className={style["left-text"]}>
+                                <div className={cx("left-text")}>
                                   <h4>
                                     Information and Safety Tips for Traveling
                                   </h4>
@@ -239,21 +237,21 @@ export const FinanceSheetCat = () => {
                                     allowed to redistribute this template ZIP
                                     file on any Free CSS collection websites.
                                   </p>
-                                  <div className={style["main-white-button"]}>
+                                  <div className={cx("main-white-button")}>
                                     <a
                                       rel="nofollow"
                                       href="https://templatemo.com/contact"
                                     >
-                                      <i className={style["fa fa-eye"]}></i>{" "}
+                                      <i className={cx("fa", "fa-eye")}></i>{" "}
                                       Read More
                                     </a>
                                   </div>
                                 </div>
                               </div>
                               <div
-                                className={`${style["col-lg-7"]} ${style["align-self-center"]}`}
+                                className={cx("col-lg-7", "align-self-center")}
                               >
-                                <div className={style["right-image"]}>
+                                <div className={cx("right-image")}>
                                   <img src={tabImg5} alt="Traveling Beach" />
                                 </div>
                               </div>
